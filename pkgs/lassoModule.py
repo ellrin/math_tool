b949042525b2bf3406f80505e4cb1b549c3d1c97
ellrin <j1542727@gmail.com>
2021-06-02 16:11:52 +0800

    update the code comment

--- pkgs/lassoModule.py
+++ pkgs/lassoModule.py
@@ -1,8 +1,7 @@
 import numpy as np
+
 class lassoRegression():
-
     # define the initial setting of lasso regression
-
     def __init__(self, learning_rate, iterations, l1_penality):
         self.learning_rate = learning_rate
         self.iterations = iterations
@@ -10,7 +9,6 @@ class lassoRegression():
 
 
     def fit(self, X, Y):
-
         # X is the model input, which is an M by N matrix
         self.Mx, self.Nx = X.shape
 
@@ -28,20 +26,17 @@ class lassoRegression():
 
 
     def update_weights(self):
-
         Y_pred = self.predict(self.X)
 
         # calculate the gradient
         dW = np.zeros(self.Nx)
 
         for j in range(self.Nx):
-
-            # since the gradient of L1 penalty have 2 possibility 
+            # since the gradient of L1 penalty has 2 possibilities 
             # (  d|Y|/dx  =  dY/dx  or  -dY/dx  )
-            # the gradient would have 2 cases according to the sign symbol:  >0 or <0
+            # the gradient would has 2 cases according to the sign symbol:  >0 or <0
 
             if self.W[j] > 0:
-
                 # the first order differential
                 dW[j] = (-(2*(self.X[:,j]).dot(self.Y - Y_pred)) 
                         + self.l1_penality) / self.Mx
@@ -50,12 +45,10 @@ class lassoRegression():
                 dW[j] = (-(2*(self.X[:,j]).dot(self.Y - Y_pred)) 
                         - self.l1_penality) / self.Mx
 
-
         dbias = -2*np.sum(self.Y - Y_pred) / self.Mx
 
         # update weights
-
-        self.W = self.W - self.learning_rate * dW
+        self.W = elf.W - self.learning_rate * dW
         self.bias = self.bias - self.learning_rate * dbias
 
         return self
